3744d0f390e1a6d930738a20112db4d0edf44001
felix <felix.widmayer@rwth-aachen.de>
2023-11-19 09:50:27 +0100

    Changed board representation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::ops;
+use std::io;
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
@@ -21,51 +21,89 @@ impl fmt::Display for CatchAllError {
 pub enum Color {
     White,
     Black,
-    None,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PieceData {
+    position: Position,
+    color: Color,
+}
+
+impl PieceData {
+    fn new(position: Position, color: Color) -> Self {
+        Self { position, color }
+    }
+}
+
+impl Default for PieceData {
+    fn default() -> Self {
+        Self {
+            position: Position::new(0, 0),
+            color: Color::White,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
 pub enum Piece {
-    Pawn(Color, bool),
-    Knight(Color),
-    Bishop(Color),
-    Rook(Color, bool),
-    Queen(Color),
-    King(Color, bool),
+    Pawn(PieceData, bool),
+    Knight(PieceData),
+    Bishop(PieceData),
+    Rook(PieceData, bool),
+    Queen(PieceData),
+    King(PieceData, bool),
+}
+
+impl Piece {
+    pub fn position(&self) -> &Position {
+        match self {
+            Piece::Pawn(data, _) => &data.position,
+            Piece::Knight(data) => &data.position,
+            Piece::Bishop(data) => &data.position,
+            Piece::Rook(data, _) => &data.position,
+            Piece::Queen(data) => &data.position,
+            Piece::King(data, _) => &data.position,
+        }
+    }
+
+    pub fn color(&self) -> &Color {
+        match self {
+            Piece::Pawn(data, _) => &data.color,
+            Piece::Knight(data) => &data.color,
+            Piece::Bishop(data) => &data.color,
+            Piece::Rook(data, _) => &data.color,
+            Piece::Queen(data) => &data.color,
+            Piece::King(data, _) => &data.color,
+        }
+    }
 }
 
 impl ToString for Piece {
     fn to_string(&self) -> String {
         match self {
-            Piece::Pawn(color, _) => match color {
+            Piece::Pawn(data, _) => match data.color {
                 Color::White => "♟".to_string(),
                 Color::Black => "♙".to_string(),
-                Color::None => panic!("Piece has no color"),
             },
-            Piece::Knight(color) => match color {
+            Piece::Knight(data) => match data.color {
                 Color::White => "♞".to_string(),
                 Color::Black => "♘".to_string(),
-                Color::None => panic!("Piece has no color"),
             },
-            Piece::Bishop(color) => match color {
+            Piece::Bishop(data) => match data.color {
                 Color::White => "♝".to_string(),
                 Color::Black => "♗".to_string(),
-                Color::None => panic!("Piece has no color"),
             },
-            Piece::Rook(color, _) => match color {
+            Piece::Rook(data, _) => match data.color {
                 Color::White => "♜".to_string(),
                 Color::Black => "♖".to_string(),
-                Color::None => panic!("Piece has no color"),
             },
-            Piece::Queen(color) => match color {
+            Piece::Queen(data) => match data.color {
                 Color::White => "♛".to_string(),
                 Color::Black => "♕".to_string(),
-                Color::None => panic!("Piece has no color"),
             },
-            Piece::King(color, _) => match color {
+            Piece::King(data, _) => match data.color {
                 Color::White => "♚".to_string(),
                 Color::Black => "♔".to_string(),
-                Color::None => panic!("Piece has no color"),
             },
         }
     }
@@ -88,11 +126,11 @@ impl FromStr for Piece {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "N" => Ok(Piece::Knight(Color::None)),
-            "B" => Ok(Piece::Bishop(Color::None)),
-            "R" => Ok(Piece::Rook(Color::None, false)),
-            "Q" => Ok(Piece::Queen(Color::None)),
-            "K" => Ok(Piece::King(Color::None, false)),
+            "N" => Ok(Piece::Knight(PieceData::default())),
+            "B" => Ok(Piece::Bishop(PieceData::default())),
+            "R" => Ok(Piece::Rook(PieceData::default(), false)),
+            "Q" => Ok(Piece::Queen(PieceData::default())),
+            "K" => Ok(Piece::King(PieceData::default(), false)),
             _ => Err(CatchAllError::BadParse),
         }
     }
@@ -200,6 +238,35 @@ impl Tokenize for &str {
     }
 }
 
+impl Tokenize for String {
+    type TokenizeResult = Result<Vec<Token>, CatchAllError>;
+
+    fn tokenize(&self) -> Self::TokenizeResult {
+        // Castle is only token that is not a char.
+        match Castle::from_str(&self) {
+            Ok(castle) => Ok(vec![Token::Castle(castle)]),
+            Err(_) => self
+                .chars()
+                .map(|ch| String::from(ch))
+                .map(|s| Token::from_str(&s))
+                .collect::<Self::TokenizeResult>(),
+        }
+    }
+}
+
+fn validate(tokens: &mut Vec<Token>) {
+    match tokens.pop() {
+        Some(Token::Piece(piece)) => println!("Promotion to {}", piece.to_string()),
+        Some(Token::Rank(Rank(rank))) => match tokens.pop() {
+            Some(Token::File(File(file))) => {
+                println!("Destination {:?}", Position::new(rank, file))
+            }
+            _ => panic!("Bad"),
+        },
+        _ => panic!("Bad"),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -217,7 +284,10 @@ mod tests {
     #[test]
     fn piece_capture() {
         let mut it = "Qxe5".tokenize().unwrap().into_iter();
-        assert_eq!(it.next(), Some(Token::Piece(Piece::Queen(Color::None))));
+        assert_eq!(
+            it.next(),
+            Some(Token::Piece(Piece::Queen(PieceData::default())))
+        );
         assert_eq!(it.next(), Some(Token::Capture));
         assert_eq!(it.next(), Some(Token::File(File(4))));
         assert_eq!(it.next(), Some(Token::Rank(Rank(4))));
@@ -227,7 +297,10 @@ mod tests {
     #[test]
     fn piece_move() {
         let mut it = "Re5".tokenize().unwrap().into_iter();
-        assert_eq!(it.next(), Some(Token::Piece(Piece::Knight(Color::None))));
+        assert_eq!(
+            it.next(),
+            Some(Token::Piece(Piece::Knight(PieceData::default())))
+        );
         assert_eq!(it.next(), Some(Token::File(File(4))));
         assert_eq!(it.next(), Some(Token::Rank(Rank(4))));
         assert_eq!(it.next(), None);
@@ -236,7 +309,10 @@ mod tests {
     #[test]
     fn file_qualified_move() {
         let mut it = "Rfe5".tokenize().unwrap().into_iter();
-        assert_eq!(it.next(), Some(Token::Piece(Piece::Knight(Color::None))));
+        assert_eq!(
+            it.next(),
+            Some(Token::Piece(Piece::Knight(PieceData::default())))
+        );
         assert_eq!(it.next(), Some(Token::File(File(5))));
         assert_eq!(it.next(), Some(Token::File(File(4))));
         assert_eq!(it.next(), Some(Token::Rank(Rank(4))));
@@ -246,7 +322,10 @@ mod tests {
     #[test]
     fn file_and_rank_qualified_move() {
         let mut it = "Rf5e5".tokenize().unwrap().into_iter();
-        assert_eq!(it.next(), Some(Token::Piece(Piece::Knight(Color::None))));
+        assert_eq!(
+            it.next(),
+            Some(Token::Piece(Piece::Knight(PieceData::default())))
+        );
         assert_eq!(it.next(), Some(Token::File(File(5))));
         assert_eq!(it.next(), Some(Token::Rank(Rank(4))));
         assert_eq!(it.next(), Some(Token::File(File(4))));
@@ -257,7 +336,10 @@ mod tests {
     #[test]
     fn piece_move_with_check() {
         let mut it = "Re5+".tokenize().unwrap().into_iter();
-        assert_eq!(it.next(), Some(Token::Piece(Piece::Knight(Color::None))));
+        assert_eq!(
+            it.next(),
+            Some(Token::Piece(Piece::Knight(PieceData::default())))
+        );
         assert_eq!(it.next(), Some(Token::File(File(4))));
         assert_eq!(it.next(), Some(Token::Rank(Rank(4))));
         assert_eq!(it.next(), Some(Token::Check));
@@ -267,7 +349,10 @@ mod tests {
     #[test]
     fn piece_move_with_checkmate() {
         let mut it = "Re5#".tokenize().unwrap().into_iter();
-        assert_eq!(it.next(), Some(Token::Piece(Piece::Knight(Color::None))));
+        assert_eq!(
+            it.next(),
+            Some(Token::Piece(Piece::Knight(PieceData::default())))
+        );
         assert_eq!(it.next(), Some(Token::File(File(4))));
         assert_eq!(it.next(), Some(Token::Rank(Rank(4))));
         assert_eq!(it.next(), Some(Token::Checkmate));
@@ -295,28 +380,21 @@ mod tests {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Position {
-    row: u8,
-    col: u8,
+    file: usize,
+    rank: usize,
 }
 
 impl Position {
-    fn new(row: u8, col: u8) -> Self {
-        let position = Self { row, col };
-        if !position.valid() {
-            panic!("Invalid position ({}, {})", row, col);
-        }
+    fn new(file: usize, rank: usize) -> Self {
+        let position = Self { file, rank };
+        assert!(position.valid(), "Invalid position ({}, {})", file, rank);
         position
     }
 
     fn valid(&self) -> bool {
-        (self.row < 8) && (self.col < 8)
-    }
-}
-
-impl Into<usize> for Position {
-    fn into(self) -> usize {
-        (8 * self.row + self.col) as usize
+        (self.file < 8) && (self.rank < 8)
     }
 }
 
@@ -334,88 +412,85 @@ pub enum Move {
 }
 
 pub struct Board {
-    fields: [Option<Piece>; 64],
+    white_pieces: Vec<Piece>,
+    black_pieces: Vec<Piece>,
 }
 
 impl Board {
+    #[rustfmt::skip]
     fn new() -> Self {
-        const INIT: Option<Piece> = None;
-        let mut board = Self { fields: [INIT; 64] };
-        // First rank.
-        board[Position::new(0, 0)] = Some(Piece::Rook(Color::White, false));
-        board[Position::new(0, 1)] = Some(Piece::Knight(Color::White));
-        board[Position::new(0, 2)] = Some(Piece::Bishop(Color::White));
-        board[Position::new(0, 3)] = Some(Piece::Queen(Color::White));
-        board[Position::new(0, 4)] = Some(Piece::King(Color::White, false));
-        board[Position::new(0, 5)] = Some(Piece::Bishop(Color::White));
-        board[Position::new(0, 6)] = Some(Piece::Knight(Color::White));
-        board[Position::new(0, 7)] = Some(Piece::Rook(Color::White, false));
-        // Second rank.
-        board[Position::new(1, 0)] = Some(Piece::Pawn(Color::White, false));
-        board[Position::new(1, 1)] = Some(Piece::Pawn(Color::White, false));
-        board[Position::new(1, 2)] = Some(Piece::Pawn(Color::White, false));
-        board[Position::new(1, 3)] = Some(Piece::Pawn(Color::White, false));
-        board[Position::new(1, 4)] = Some(Piece::Pawn(Color::White, false));
-        board[Position::new(1, 5)] = Some(Piece::Pawn(Color::White, false));
-        board[Position::new(1, 6)] = Some(Piece::Pawn(Color::White, false));
-        board[Position::new(1, 7)] = Some(Piece::Pawn(Color::White, false));
-        // Seventh rank.
-        board[Position::new(6, 0)] = Some(Piece::Pawn(Color::Black, false));
-        board[Position::new(6, 1)] = Some(Piece::Pawn(Color::Black, false));
-        board[Position::new(6, 2)] = Some(Piece::Pawn(Color::Black, false));
-        board[Position::new(6, 3)] = Some(Piece::Pawn(Color::Black, false));
-        board[Position::new(6, 4)] = Some(Piece::Pawn(Color::Black, false));
-        board[Position::new(6, 5)] = Some(Piece::Pawn(Color::Black, false));
-        board[Position::new(6, 6)] = Some(Piece::Pawn(Color::Black, false));
-        board[Position::new(6, 7)] = Some(Piece::Pawn(Color::Black, false));
-        // Eigth rank.
-        board[Position::new(7, 0)] = Some(Piece::Rook(Color::Black, false));
-        board[Position::new(7, 1)] = Some(Piece::Knight(Color::Black));
-        board[Position::new(7, 2)] = Some(Piece::Bishop(Color::Black));
-        board[Position::new(7, 3)] = Some(Piece::Queen(Color::Black));
-        board[Position::new(7, 4)] = Some(Piece::King(Color::Black, false));
-        board[Position::new(7, 5)] = Some(Piece::Bishop(Color::Black));
-        board[Position::new(7, 6)] = Some(Piece::Knight(Color::Black));
-        board[Position::new(7, 7)] = Some(Piece::Rook(Color::Black, false));
+        let mut board = Self {
+            white_pieces: Vec::new(),
+            black_pieces: Vec::new(),
+        };
+
+        board.white_pieces.push(Piece::Rook(PieceData::new(Position::new(0, 0), Color::White), false));
+        board.white_pieces.push(Piece::Knight(PieceData::new(Position::new(1, 0), Color::White)));
+        board.white_pieces.push(Piece::Bishop(PieceData::new(Position::new(2, 0), Color::White)));
+        board.white_pieces.push(Piece::Queen(PieceData::new(Position::new(3, 0), Color::White)));
+        board.white_pieces.push(Piece::King(PieceData::new(Position::new(4, 0), Color::White), false));
+        board.white_pieces.push(Piece::Bishop(PieceData::new(Position::new(5, 0), Color::White)));
+        board.white_pieces.push(Piece::Knight(PieceData::new(Position::new(6, 0), Color::White)));
+        board.white_pieces.push(Piece::Rook(PieceData::new(Position::new(7, 0), Color::White), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(0, 1), Color::White), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(1, 1), Color::White), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(2, 1), Color::White), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(3, 1), Color::White), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(4, 1), Color::White), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(5, 1), Color::White), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(6, 1), Color::White), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(7, 1), Color::White), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(0, 6), Color::Black), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(1, 6), Color::Black), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(2, 6), Color::Black), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(3, 6), Color::Black), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(4, 6), Color::Black), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(5, 6), Color::Black), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(6, 6), Color::Black), false));
+        board.white_pieces.push(Piece::Pawn(PieceData::new(Position::new(7, 6), Color::Black), false));
+        board.white_pieces.push(Piece::Rook(PieceData::new(Position::new(0, 7), Color::Black), false));
+        board.white_pieces.push(Piece::Knight(PieceData::new(Position::new(1, 7), Color::Black)));
+        board.white_pieces.push(Piece::Bishop(PieceData::new(Position::new(2, 7), Color::Black)));
+        board.white_pieces.push(Piece::Queen(PieceData::new(Position::new(3, 7), Color::Black)));
+        board.white_pieces.push(Piece::King(PieceData::new(Position::new(4, 7), Color::Black), false));
+        board.white_pieces.push(Piece::Bishop(PieceData::new(Position::new(5, 7), Color::Black)));
+        board.white_pieces.push(Piece::Knight(PieceData::new(Position::new(6, 7), Color::Black)));
+        board.white_pieces.push(Piece::Rook(PieceData::new(Position::new(7, 7), Color::Black), false));
+
         board
     }
 }
 
-impl ops::IndexMut<Position> for Board {
-    fn index_mut(&mut self, position: Position) -> &mut Self::Output {
-        let i: usize = position.into();
-        &mut self.fields[i]
-    }
-}
-
-impl ops::Index<Position> for Board {
-    type Output = Option<Piece>;
-
-    fn index(&self, position: Position) -> &Self::Output {
-        let i: usize = position.into();
-        &self.fields[i]
-    }
-}
-
 impl fmt::Display for Board {
+    #[rustfmt::skip]
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        (0..8).into_iter().rev().for_each(|i| {
-            let row = (0..8).into_iter().fold(String::from(""), |mut acc, j| {
-                acc.push_str(&format!(
-                    "{}",
-                    self[Position::new(i, j)].as_ref().map_or(
-                        match i + j {
-                            sum if sum % 2 == 0 => "□".to_string(),
-                            sum if sum % 2 != 0 => "■".to_string(),
-                            _ => panic!("Unreachable"),
-                        },
-                        |piece| piece.to_string()
-                    )
-                ));
-                acc
-            });
-            write!(f, "{}\n", row).unwrap();
+        let mut board: [[String; 8]; 8] = [
+            ["□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string()],
+            ["■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string()],
+            ["□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string()],
+            ["■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string()],
+            ["□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string()],
+            ["■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string()],
+            ["□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string()],
+            ["■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string(), "■".to_string(), "□".to_string()],
+        ];
+
+        self.white_pieces.iter().for_each(|p| {
+            let Position { file: i, rank: j } = *p.position();
+            board[j][i] = p.to_string();
         });
+        self.black_pieces.iter().for_each(|p| {
+            let Position { file: i, rank: j } = *p.position();
+            board[j][i] = p.to_string();
+        });
+
+        board.iter().rev().for_each(|rank| {
+            let line = rank
+                .iter()
+                .fold("".to_string(), |acc, square| acc + &square.to_string());
+            writeln!(f, "{}", line).unwrap();
+        });
+
         Ok(())
     }
 }
@@ -423,5 +498,36 @@ impl fmt::Display for Board {
 fn main() {
     let board = Board::new();
 
+    println!("The game has started.");
     println!("{}", board);
+
+    let mut color = Color::White;
+
+    loop {
+        let mut turn = String::new();
+
+        io::stdin()
+            .read_line(&mut turn)
+            .ok()
+            .expect("Failed to read line.");
+
+        turn.pop();
+
+        let tokens = turn.tokenize();
+
+        if let Ok(mut tokens) = tokens {
+            validate(&mut tokens);
+        }
+
+        color = match color {
+            Color::White => {
+                println!("White turn {}.", turn);
+                Color::Black
+            }
+            Color::Black => {
+                println!("Black turn turn {}.", turn);
+                Color::White
+            }
+        };
+    }
 }
