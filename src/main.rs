06bd1b811e6152acd92cfd803ee1ef2a60c6e855
felix <felix.widmayer@rwth-aachen.de>
2023-11-28 21:34:54 +0100

    Todos

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod board;
 mod error;
+mod game;
 mod gui;
 mod r#move;
 mod piece;
 mod position;
-mod game;
 
 use gui::Gui;
 use iced::window;
@@ -12,6 +12,14 @@ use iced::Sandbox;
 use iced::Settings;
 
 pub fn main() -> iced::Result {
+    todo!("Ensure that the king cannot castle through check.");
+    todo!("Correctly wind back moves by also restoring piece state (maybe use counter that ticks up on piece move rather than state enum).");
+    todo!("Determine checkmate and stalemate (brute-force check all reachable fields for all pieces of king's color).");
+    todo!("Resolve en-passant.");
+    todo!("Handle promotion.");
+    todo!("Reduce responsibilities of board");
+    todo!("Handle draw by three-fold repetition (maybe hash the board state), by insufficient material, by 50 move rule (simple capture counter)");
+
     Gui::run(Settings {
         window: window::Settings {
             size: (800, 800),
